a04fa2eca1948fd434f05cd77de8e6e5322b2dab
Angela Zhou <angela.zhou1@ocsbstudent.ca>
2018-09-12 09:00:02 -0400

    Virtual Buddy Complete, brought back Virtual Pet

---
+++ ICS4UC_Unit1/src/activity3/VirtualBuddy.java
@@ -0,0 +1,96 @@
+package activity3;
+
+import javafx.scene.control.Button;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class VirtualBuddy extends Application {
+	
+	static final int GAP = 15;
+	static final int LARGE_FONT  = 40;
+	static final int MEDIUM_FONT = 25;
+	static final int SMALL_FONT  = 12;
+	
+	//Output UI Control
+	ListView<String> lstResponse;
+
+	@Override
+	public void start(Stage myStage) throws Exception {
+
+        GridPane root = new GridPane();
+        
+        root.setHgap(GAP);
+        root.setVgap(GAP);
+        root.setPadding(new Insets(GAP, GAP, GAP, GAP));
+        
+        /**
+         * Titles
+         */
+        Label lblTitle = new Label("Virtual Buddy");
+        lblTitle.setFont(Font.font(LARGE_FONT));
+        root.add(lblTitle, 0, 0, 2, 1);  //pos (0,0), colspan = 2
+        
+        Label lblSubTitle = new Label("Hi! How are you?");
+        lblSubTitle.setFont(Font.font(MEDIUM_FONT));
+        root.add(lblSubTitle, 0, 1, 2, 1); //pos (0,1), colspan = 2
+        
+        /**
+         * Buddy Image
+         */
+        ImageView imgBuddy = new ImageView(getClass().getResource("images/penguin.png").toString());
+        root.add(imgBuddy, 0, 2, 1, 5);  //pos (0,2), rowspan = 5
+        
+        /**
+         * Buttons
+         */
+        Button btnGreet = new Button("Greet");
+        btnGreet.setFont(Font.font(SMALL_FONT));
+        root.add(btnGreet, 1, 2); // pos (1, 2)
+        
+        Button btnFact = new Button("Fun Fact");
+        btnFact.setFont(Font.font(SMALL_FONT));
+        root.add(btnFact, 1, 3); // pos (1, 3)
+        
+        Button btnRiddle = new Button("Riddle");
+        btnRiddle.setFont(Font.font(SMALL_FONT));
+        root.add(btnRiddle, 1, 4); // pos (1, 4)
+        
+        Button btnTry = new Button("Try This!");
+        btnTry.setFont(Font.font(SMALL_FONT));
+        root.add(btnTry, 1, 5); // pos (1, 5)
+        
+        /**
+         * Response
+         */
+        Label lblResponse = new Label();
+        lblResponse.setFont(Font.font(MEDIUM_FONT));
+        root.add(lblResponse, 0, 9, 8, 1);  //pos (9,0), colspan = 8
+        
+        /**
+    	 * Event Handler
+    	 */
+        btnGreet.setOnAction (event -> lblResponse.setText("My name is Eddie, what is yours?"));
+        btnFact.setOnAction  (event -> lblResponse.setText("I'm a bird that can't fly!"));
+        btnRiddle.setOnAction(event -> lblResponse.setText("How do penguins drink?\r\n" + 
+        												   "Out of beak-ers!"));
+        btnTry.setOnAction   (event -> lblResponse.setText("Try not to think about penguins."));
+        
+        Scene scene = new Scene(root);
+
+        myStage.setTitle("Virtual Buddy");
+        myStage.setScene(scene);
+        myStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
--- ICS4UC_Unit1/src/activity5/VirtualPet.java
+++ ICS4UC_Unit1/src/activity5/VirtualPet.java
@@ -1,7 +1,5 @@
 package activity5;
 
-import javafx.scene.control.Button;
-import javafx.scene.Group;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -10,15 +8,13 @@ import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class VirtualPet extends Application {
 	
 	static final int GAP = 15;
-	static final int LARGE_FONT  = 40;
-	static final int MEDIUM_FONT = 25;
-	static final int SMALL_FONT  = 12;
+	static final int LARGE_FONT = 30;
+	static final int SMALL_FONT = 14;
 	
 	//Output UI Control
 	ListView<String> lstResponse;
@@ -32,48 +28,17 @@ public class VirtualPet extends Application {
         root.setVgap(GAP);
         root.setPadding(new Insets(GAP, GAP, GAP, GAP));
         
-        /**
-         * Titles
-         */
         Label lblTitle = new Label("Virtual Pet");
         lblTitle.setFont(Font.font(LARGE_FONT));
         root.add(lblTitle, 0, 0, 2, 1);  //pos (0,0), colspan = 2
         
-        Label lblSubTitle = new Label("Hi! How are you?");
-        lblSubTitle.setFont(Font.font(MEDIUM_FONT));
-        root.add(lblSubTitle, 0, 1, 2, 1); //pos (0,1), colspan = 2
+        ImageView imgPet = new ImageView(getClass().getResource("images/boogle.jpg").toString());
+        root.add(imgPet, 0, 1, 1, 5);  //pos (0,1), rowspan = 5
         
-        /**
-         * Pet Image
-         */
-        ImageView imgPet = new ImageView(getClass().getResource("images/penguin.png").toString());
-        root.add(imgPet, 0, 2, 1, 5);  //pos (0,2), rowspan = 5
+        lstResponse = new ListView<String>();
+        root.add(lstResponse, 2, 0, 1, 6);  //pos (2,0), rowspan = 6
         
-        /**
-         * Buttons
-         */
-        Button btnGreet = new Button("Greet");
-        btnGreet.setFont(Font.font(SMALL_FONT));
-        root.add(btnGreet, 1, 2); // pos (1, 2)
         
-        Button btnFact = new Button("Fun Fact");
-        btnFact.setFont(Font.font(SMALL_FONT));
-        root.add(btnFact, 1, 3); // pos (1, 3)
-        
-        Button btnRiddle = new Button("Riddle");
-        btnRiddle.setFont(Font.font(SMALL_FONT));
-        root.add(btnRiddle, 1, 4); // pos (1, 4)
-        
-        Button btnTry = new Button("Try This!");
-        btnTry.setFont(Font.font(SMALL_FONT));
-        root.add(btnTry, 1, 5); // pos (1, 5)
-        
-        /**
-         * Response
-         */
-        Label lblResponse = new Label();
-        lblResponse.setFont(Font.font(MEDIUM_FONT));
-        root.add(lblResponse, 0, 6, 2, 1);  //pos (2,0), colspan = 2
         
         Scene scene = new Scene(root);
 
@@ -81,16 +46,6 @@ public class VirtualPet extends Application {
         myStage.setScene(scene);
         myStage.show();
 	}
-	
-	/**
-	 * Event Handler
-	 */
-//	btnGreet.setOnAction(new EventHandler<ActionEvent>() {
-//		@Override
-//		public void handle(ActionEvent event) {
-//			lblResponse.setText("My name is Eddie. What is yours?");
-//		}
-//	});
 
 	public static void main(String[] args) {
 		launch(args);
