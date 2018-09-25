2f8d8e8c26b03d45d9ee050a532d6ceab238ac3c
Angela Zhou <angela.zhou1@ocsbstudent.ca>
2018-09-25 09:30:58 -0400

    Almost complete activity 5

--- ICS4UC_Unit1/src/activity5/Boogle.java
+++ ICS4UC_Unit1/src/activity5/Boogle.java
@@ -1,17 +1,21 @@
 package activity5;
 
 public class Boogle {
-	private String name; 
-	private int happiness  = 50;
-	private int tiredness  = 0;
-	private int hunger     = 0;
+	private String name;
+	private String prompt;
+	private int happiness  = 0;
+	private int tiredness  = 10;
+	private int hunger     = 10;
 	private int energy     = 0;
 	
 	public Boogle() {
 		setName("Arnold");
-		
+		setPrompt("Try and make me happy!" + "\nHappiness Level: " + happiness + "\nTiredness Level: " + tiredness);
 	}
 
+	/**
+	 * Getters and setters
+	 */
 	public String getName() {
 		return name;
 	}
@@ -20,25 +24,46 @@ public class Boogle {
 		this.name = name;
 	}
 	
-	public String feed(String food, int happiness, int energy) {
-		
-		return "Yum, that was a delicious " + food + "!";
+	public String getPrompt() {
+		return prompt;
+	}
+
+	public void setPrompt(String prompt) {
+		this.prompt = prompt;
 	}
 	
-	public String play(String game) {
-		return "I love playing " + game;
+	/**
+	 * Boogle interaction methods
+	 */
+	public String feed(String food, int energy) {
+		happiness += energy;
+		tiredness -= energy;
+		return "Yum, that was a delicious " + food + "!"
+				+ "\nHappiness Level: " + happiness + "\nTiredness Level: " + tiredness;
 	}
 	
-	public String walk(String place) {
-		return "I want to go for a walk in " + place;
+	public String sleep(String bed, int energy) {
+		happiness += energy;
+		tiredness -= energy;
+		return "The " + bed + " is very comfortable!"
+				+ "\nHappiness Level: " + happiness + "\nTiredness Level: " + tiredness;
 	}
 	
-	public String groom(String groom) {
-		return groom + " are the best!";
+	public String play(String game, int hunger) {
+		tiredness += hunger;
+		return "I love playing " + game + "!"
+				+ "\nHappiness Level: " + happiness + "\nTiredness Level: " + tiredness;
 	}
 	
-	public String sleep(String bed) {
-		return "The " + bed + " is very comfortable!";
+	public String walk(String place, int hunger) {
+		tiredness += hunger;
+		return "I want to go for a walk in " + place + "!"
+				+ "\nHappiness Level: " + happiness + "\nTiredness Level: " + tiredness;
 	}
 	
+	public String groom(String groom, int energy) {
+		happiness += energy;
+		return groom + " are the best!"
+				+ "\nHappiness Level: " + happiness + "\nTiredness Level: " + tiredness;
+	}	
 }
--- ICS4UC_Unit1/src/activity5/VirtualPet.java
+++ ICS4UC_Unit1/src/activity5/VirtualPet.java
@@ -13,12 +13,19 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.scene.control.TextInputControl;
 
 public class VirtualPet extends Application {
 
 	static final int GAP = 15;
 	static final int LARGE_FONT = 30;
 	static final int SMALL_FONT = 14;
+	
+	public final String feed  = "Insert a food type (eg. muffin)";
+	public final String sleep = "Insert a place to sleep (eg. couch)";
+	public final String play  = "Insert a game to play (eg. soccer)";
+	public final String walk  = "Insert a place to walk (eg. the park)";
+	public final String groom = "Insert a method of grooming (eg. bubble baths)";
 
 	// Global variables
 	Boogle myPet;
@@ -43,46 +50,62 @@ public class VirtualPet extends Application {
 		root.add(imgPet, 0, 1, 1, 5);  //pos (0,1), rowspan = 5
 
 		lstResponse = new ListView<String>();
-		root.add(lstResponse, 4, 0, 1, 10);  //pos (4,0), rowspan = 10
+		root.add(lstResponse, 10, 0, 1, 10);  //pos (10,0), rowspan = 10
 
 		myPet = new Boogle();
 		lstResponse.getItems().add("Hello, my name is " + myPet.getName() + "!");
+		lstResponse.getItems().add(myPet.getPrompt());
 
 		/**
 		 *  Input
 		 */
 		// input layout
 		VBox vbxInput = new VBox(GAP);
-		root.add(vbxInput, 0, 6, 6, 1);
+		root.add(vbxInput, 0, 6, 6, 1); // pos (0,6) colspan = 6
 
 		// feed button
 		Button btnFeed = new Button("Feed");
 		btnFeed.setFont(Font.font(SMALL_FONT));
-		
+
 		TextField txtFeed = new TextField();
+		txtFeed.setPromptText(feed);
 		HBox hbxFeed = new HBox(GAP, txtFeed, btnFeed);
 		vbxInput.getChildren().add(hbxFeed);
-		
+
 		btnFeed.setOnAction(event -> feedBoogle(txtFeed.getText()));
 
 		// play button
 		Button btnPlay = new Button("Play");
 		btnPlay.setFont(Font.font(SMALL_FONT));
-		
+
 		TextField txtPlay = new TextField();
+		txtPlay.setPromptText(feed);
 		HBox hbxPlay = new HBox(GAP, txtPlay, btnPlay);
 		vbxInput.getChildren().add(hbxPlay);
-		
+
 		btnPlay.setOnAction(event -> playBoogle(txtPlay.getText()));
+
+		// sleep button
+		Button btnSleep = new Button("Sleep");
+		btnSleep.setFont(Font.font(SMALL_FONT));
+		
+		TextField txtSleep = new TextField();
+		txtSleep.setPromptText(sleep);
+		HBox hbxSleep = new HBox(GAP, txtSleep, btnSleep);
+		vbxInput.getChildren().add(hbxSleep);
+
+		btnSleep.setOnAction(event -> sleepBoogle(txtSleep.getText()));
+
 
 		// walk button
 		Button btnWalk = new Button("Walk");
 		btnWalk.setFont(Font.font(SMALL_FONT));
 
 		TextField txtWalk = new TextField();
+		txtWalk.setPromptText(walk);
 		HBox hbxWalk = new HBox(GAP, txtWalk, btnWalk);
 		vbxInput.getChildren().add(hbxWalk);
-		
+
 		btnWalk.setOnAction(event -> walkBoogle(txtWalk.getText()));
 
 		// groom button
@@ -90,20 +113,11 @@ public class VirtualPet extends Application {
 		btnGroom.setFont(Font.font(SMALL_FONT));
 
 		TextField txtGroom = new TextField();
+		txtGroom.setPromptText(groom);
 		HBox hbxGroom = new HBox(GAP, txtGroom, btnGroom);
 		vbxInput.getChildren().add(hbxGroom);
-		
+
 		btnGroom.setOnAction(event -> groomBoogle(txtGroom.getText()));
-
-		// sleep button
-		Button btnSleep = new Button("Sleep");
-		btnSleep.setFont(Font.font(SMALL_FONT));
-
-		TextField txtSleep = new TextField();
-		HBox hbxSleep = new HBox(GAP, txtSleep, btnSleep);
-		vbxInput.getChildren().add(hbxSleep);
-		
-		btnSleep.setOnAction(event -> sleepBoogle(txtSleep.getText()));
 
 		Scene scene = new Scene(root);
 
@@ -118,22 +132,22 @@ public class VirtualPet extends Application {
 	}
 
 	private void playBoogle(String game) {
-		String boogleResponse = myPet.play(game);
+		String boogleResponse = myPet.play(game, 10);
 		lstResponse.getItems().add(boogleResponse);
 	}
-	
+
 	private void walkBoogle(String place) {
-		String boogleResponse = myPet.walk(place);
+		String boogleResponse = myPet.walk(place, 10);
 		lstResponse.getItems().add(boogleResponse);
 	}
-	
+
 	private void groomBoogle(String groom) {
-		String boogleResponse = myPet.groom(groom);
+		String boogleResponse = myPet.groom(groom, 10);
 		lstResponse.getItems().add(boogleResponse);
 	}
-	
+
 	private void sleepBoogle(String bed) {
-		String boogleResponse = myPet.sleep(bed);
+		String boogleResponse = myPet.sleep(bed, 10);
 		lstResponse.getItems().add(boogleResponse);
 	}
 
