22abd7c6b21fbda9d5e80ccab447537516bcab21
Angela Zhou <angela.zhou1@ocsbstudent.ca>
2018-10-17 17:57:37 -0400

    Copied & pasted Tetris code

--- ICS4UC_Unit1/src/assignment/Tetris.java
+++ ICS4UC_Unit1/src/assignment/Tetris.java
@@ -1,5 +1,59 @@
 package assignment;
 
-public class Tetris {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Tetris extends Application {
+
+	public static final int SCREEN_WIDTH = 800;
+	public static final int SCREEN_HEIGHT = 600;
+
+	Group root;
+	TetrisBlock block;
+	Text upcoming;
+
+	@Override
+	public void start(Stage myStage) throws Exception {
+
+		root = new Group();
+		Scene scene = new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+		block = getBlock();
+
+		upcoming = new Text(5, 15, block.toString());
+		root.getChildren().add(upcoming);
+
+		scene.setOnMouseClicked(event -> placeBlock(event));
+
+		myStage.setTitle("Tetris");
+		myStage.setScene(scene);
+		myStage.show();
+
+	}
+
+	private void placeBlock(MouseEvent event) {
+
+		//snap block to grid
+		int X = ((int) event.getX() / TetrisBlock.SIZE) * TetrisBlock.SIZE;
+		int Y = ((int) event.getY() / TetrisBlock.SIZE) * TetrisBlock.SIZE;
+
+		//TODO: set the location to X and Y and draw the block. 
+
+		//TODO: get a new block and display its description in the Text object
+		
+	}
+
+	private TetrisBlock getBlock() {
+		// TODO: Implement method to create a block at random and add to the scene graph
+		return null;
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 
 }
